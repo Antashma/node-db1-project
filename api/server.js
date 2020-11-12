0114387f39e426fb7b7f79f2d9b945600aa50441
Samantha Goodman <antashma@gmail.com>
2020-11-12 13:57:03 -0500

    added query task answsers to queries.sql

--- api/server.js
+++ api/server.js
@@ -6,4 +6,12 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send(
+        `<h1>Welcome to Sam G's Node-DB1 Project</h1>
+        <p>Please open insomnia or postman to interact!</p>
+        `
+    )
+})
+
 module.exports = server;
